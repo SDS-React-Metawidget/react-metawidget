b525cb710d7314d21c6d54d4dd137b1e7e48d10e
Tim Porritt <cimera2@gmail.com>
2016-10-26 15:59:42 +1100

    Removed unset readOnly from metawidget creation

--- files/js/code.js
+++ files/js/code.js
@@ -3,7 +3,6 @@ ReactDOM.render(
         //toInspect={person}
         inspector={new metawidget.inspector.JsonSchemaInspector(schema)}
         appendWidgetProcessors={new metawidget.react.widgetprocessor.ReactBindingProcessor()}
-        readOnly
     />,
     document.getElementById("metawidget")
 )
